2be820a3ddfb9de608f95edb4e5dbf60988b9078
PaulGyan <gyanpaulkwasi@gmail.com>
2024-06-20 05:29:52 +0000

    Mathematical operations were performed

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+number1 = 10
+number2 = 5
+
+addition = number1 + number2
+print("Addition of 10 and 5 is",addition)
+
+subtraction = number1 - number2
+print("Subtraction of 10 and 5 is",subtraction)
+
+multiplication = number1 * number2
+print("Multiplication of 10 and 5 is",multiplication)
