6084776b81cb484f31c134cde1cd20c8fade2d31
PaulGyan <gyanpaulkwasi@gmail.com>
2024-07-12 11:54:21 +0000

    Done

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -24,6 +24,6 @@ class BankAccount:
 # Example usage:
 if __name__ == "__main__":
     my_account = BankAccount(initial_balance=100)
-    my_account.deposit(200)
-    my_account.withdraw(150)
+    my_account.deposit(67)
+    my_account.withdraw(50)
     my_account.display_balance()
