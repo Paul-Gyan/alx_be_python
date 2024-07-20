43756cceeb258760a56b3838fbf8f108f3869f5f
PaulGyan <gyanpaulkwasi@gmail.com>
2024-07-20 13:52:32 +0000

    Added new file

--- oop/library_system.py
+++ oop/library_system.py
@@ -17,7 +17,7 @@ class Library:
         self.books = []
     def add_book(self,book):
         self.books.append(book)
-    def list_book(self):
+    def list_books(self):
         for book in self.books:
             print(book)
    
